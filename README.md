d949bfa9f3061fc804a8ab51d18c9107bab56cf5
Sebastian Cohnen <tisba@users.noreply.github.com>
2023-05-27 21:06:40 +0200

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# MiniRacer Debugging
+
+This is my personal, kitchensink tool collection to help reproducing issues with [mini_racer](https://github.com/rubyjs/mini_racer).
---
+++ cases/minimal_stable-0.6.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Run with:
+#   ruby cases/minimal_stable-0.6.rb
+#   -or via docker-
+#   docker run -it --rm -v "$(pwd)"/cases:/src -w /src ruby:3.2.2 ruby /src/minimal_stable-0.6.rb
+
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+
+  gem "mini_racer", github: "rubyjs/mini_racer", branch: "stable-0.6"
+end
+
+require "libv8-node"
+require "rbconfig"
+
+puts "RbConfig::CONFIG['LIBS']: #{RbConfig::CONFIG["LIBS"]}"
+puts "RUBY_VERSION : #{RUBY_VERSION}"
+puts "RUBY_PLATFORM: #{RUBY_PLATFORM}"
+puts "MiniRacer::VERSION: #{MiniRacer::VERSION}"
+puts "MiniRacer::LIBV8_NODE_VERSION: #{MiniRacer::LIBV8_NODE_VERSION}"
+puts "Libv8::Node::VERSION: #{Libv8::Node::VERSION}"
+puts "Libv8::Node::NODE_VERSION: #{Libv8::Node::NODE_VERSION}"
+puts "Libv8::Node::LIBV8_VERSION: #{Libv8::Node::LIBV8_VERSION}"
+
+ctx = MiniRacer::Context.new
+
+puts ctx.eval("1+1")
